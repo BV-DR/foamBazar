a5536b91f351f4fac4ca8c51eefc256a7d84c83d
Charles Monroy <charles.monroy@bureauveritas.com>
2017-01-27 10:30:17 +0100

    Addition of an automatic creation of waveProbList

--- pythonScripts/waveProbes.py
+++ pythonScripts/waveProbes.py
@@ -3,6 +3,8 @@
 """
 
 from compatOF import surfaceElevation, alpha
+from PyFoam.RunDictionary.ParsedParameterFile import WriteParameterFile
+
 
 def setWaveProbes( waveProbesList , version = "foamStar" , writeProbesInterval = 0.1 ) :
 
@@ -21,3 +23,28 @@ def setWaveProbes( waveProbesList , version = "foamStar" , writeProbesInterval =
    d[surfaceElevation[version]]["sets"] =   [ "p_{0:05} {{start ({1:} {2:} {3:}); end ({1:} {2:} {4:});  type face; axis z; nPoints {5:}; }}".format( i ,  *p )  for i, p  in enumerate(waveProbesList)  ]
 
    return d
+
+
+def createLinearWaveProbesList( xMin, xMax, nX, y, zMin, Zmax, nZ) :
+	waveProbesList = []
+	deltaX=(xMax-xMin)/(nX-1)
+	for i in range(0,nX):
+		waveProbesList.append([xMin+i*deltaX, y, zMin, Zmax, nZ])
+	return waveProbesList
+
+if __name__ == "__main__" :
+
+   # works with tuples or with lists
+   #waveProbesList = ( (10.,0.,-1.,+1 , 100) , (15.,0.,-1.,+1 , 100) )
+   waveProbesList = [ [10.,0.,-1.,+1 , 100] , [15.,0.,-1.,+1 , 100] ]
+   print waveProbesList
+
+   waveProbesList=createLinearWaveProbesList( -100.0, 100.0, 201, 0.05, -3.0, 3.0, 100)
+   print waveProbesList
+   d = setWaveProbes ( waveProbesList, "foamStar" , writeProbesInterval = 0.01 )
+
+   waveProbFile = WriteParameterFile("waveProb.inc")
+   waveProbFile["functions"] = d
+   waveProbFile.writeFile()
+
+
