dac94d1d3bcbcd826d6e40938eb51bcd3ba87ce5
Guillaume de Hauteclocque <guillaume.de-hauteclocque@bureauveritas.com>
2017-02-03 16:49:55 +0100

    waveProbesPP.py : Read with pandas.read_csv and return DataFrame (to be consistent with TimeSignal shared code update)

--- pythonScripts/waveProbesPP.py
+++ pythonScripts/waveProbesPP.py
@@ -3,30 +3,35 @@
   Post-process foamStar wave probes  data file. ("surfaceElevation.dat")
 
 """
-
-
 from  matplotlib import pyplot as plt
 from  matplotlib import animation
 import numpy as np
+import pandas as pd
+import os
 
 
-def makeAnimation(filename , movieName = None, nShaddow = 20, xRatio= 1.0) :
+def makeAnimation(filename , movieName = None, nShaddow = 20, xRatio= 1.0, version = "foamStar" , rate = 1) :
    """
       Make a 2D animation of the wave field.
    """
    print "Making animation file : " , movieName
    nShaddow = max(1,nShaddow)
-   data = np.loadtxt(filename)
-   with open(filename , "r") as f :
-      f.readline() ; f.readline() ; xVal = [float(x) for x in f.readline().split()[2:]]
+   
+   data = pd.read_csv(filename, comment = "#" , header=None , delim_whitespace=True, dtype = float).as_matrix()
 
-   #Remove x = 0.0
-   if 0.0 in xVal :
-      i = xVal.index(0.0)
-      xVal.pop(i)
-      data = np.delete(data, i+1 , axis = 1)
-   xVal = np.array(xVal)
-   xVal /= xRatio
+   if version == "foamStar" :
+      with open(filename , "r") as f :
+         f.readline() ; f.readline() ; xVal = [float(x) for x in f.readline().split()[2:]]
+      #Remove x = 0.0
+      if 0.0 in xVal :
+         i = xVal.index(0.0)
+         xVal.pop(i)
+         data = np.delete(data, i+1 , axis = 1)
+      xVal = np.array(xVal)
+      xVal /= xRatio
+
+   else :
+      xVal = np.linspace(0,1 , len(data[1,:]) -1 )
 
    fig, ax = plt.subplots()
    ls = []
@@ -44,10 +49,10 @@ def makeAnimation(filename , movieName = None, nShaddow = 20, xRatio= 1.0) :
    ax.set_ylabel("Elevation(m)")
 
    def run(itime):
-     ax.set_title("{}s".format(data[itime,0]) )
+     ax.set_title("{}s".format(data[itime*rate,0]) )
      for s in range(nShaddow):
         if itime > s :
-           ls[s].set_data( xVal , data[itime-s,1:] )
+           ls[s].set_data( xVal , data[ rate*(itime - s),1:] )
      return ls
 
    ani = animation.FuncAnimation( fig , run, range(len(data)), blit=True, interval=30, repeat=False)
@@ -59,12 +64,30 @@ def makeAnimation(filename , movieName = None, nShaddow = 20, xRatio= 1.0) :
       ani.save( movieName +'.mp4',writer=mywriter)
 
 
-def getTimeSignal(filename) :
-   from TimeDomain import TimeSignals as ts
-   with open(filename , "r") as f :
-      f.readline() ; f.readline() ; label = f.readline().split()[2:]
-   d = np.loadtxt(filename)
-   data = d[:,1:]
-   data.shape = -1 , len(label)
-   xAxis = d[:,0]
-   return ts.TimeSignals( data , xAxis = xAxis , columnTitles = label )
+def getTimeSignal(filename, version = "foamStar", label = None) :
+   if label is None :
+      with open(filename , "r") as f :
+         if version == "foamStar" :
+            f.readline() ; f.readline() ; label = f.readline().split()[2:]
+         else :
+            label = [ "{:0002}".format(i+1) for i in range(len(f.readline().split())-1) ]
+
+   if not os.path.exists(filename) :
+      print filename, 'does not exist'
+   return  pd.read_csv( filename , header = 0 , names = label , index_col = 0 , engine = "c"  , delim_whitespace = True , comment = "#")
+
+
+if __name__ == "__main__" :
+
+   print "Run"
+   #makeAnimation( r"\\10.67.24.192\bigr\openFoam\2Dwave\Run_revI\g1_foamExtend_Speed_0.0\surfaceElevation.dat" ,  rate = 5 , version = "foamExtend" , nShaddow = 0)
+
+   test = getTimeSignal ( r"\\10.67.24.192\bigr\openFoam\2Dwave\Run_rev1\g1_foamStar_Speed_0.0\postProcessing\waveProbes\0\surfaceElevation.dat" )
+
+   try :
+      from pyplotTools import dfSlider
+      test.columns = map(float , test.columns)
+      print test
+      dfSlider(test)
+   except :
+      pass
