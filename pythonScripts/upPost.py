1860050ab52d32c50578fd1d2ef2573779987a4b
Alexis BENHAMOU <abenhamou@drhpc_mail>
2017-06-27 08:35:52 +0200

    upPost.py: update up-crossing analysis

--- pythonScripts/upPost.py
+++ pythonScripts/upPost.py
@@ -34,25 +34,25 @@ for ts in inp_list: print '- '+ts
 while True:
     input = raw_input('>>>')
     if input in inp_list:
-        print input
         break
     elif input in ['exit','break','cancel']:
         sys.exit()
     else:
         print 'Input is invalid, please select time serie from list.'
 
-# data = pd.read_table(os.path.join(dir_path,input+'.dat'),sep=' ',comment='#',header=None)
-# data = openFoam.openFoamReader(os.path.join(dir_path,input+'.dat'))
-data = openFoam.foamStarMotion(os.path.join(dir_path,input+'.dat'))
+if input in ['fx','fy','fz','mx','my','mz']:
+    data = openFoam.foamStarMotion(os.path.join(dir_path,input+'.dat'))
+else:
+    data = openFoam.openFoamReader(os.path.join(dir_path,input+'.dat'))
 
-# print data[['3']]
+resu = pd.DataFrame()
+for idx in data.columns.values:
+    mini = min(data.loc[:,idx])
+    maxi = max(data.loc[:,idx])
+    if mini!=maxi:
+        res = upCross.upCrossMinMax(data.loc[:,idx])
+        tmp = pd.DataFrame( data={ input+'_'+idx+'_min': res.Minimum, input+'_'+idx+'_max': res.Maximum} )
+        resu = pd.concat([resu,tmp],axis=1)
 
-upCross.upCrossMinMax(data.loc[:,'3'])
-
-# for col in xrange(data.shape[1]):
-    # res = upCross.upCrossMinMax(data[[col+1]])
-# print res.values
-
-
-
-
+resu = resu.transpose()
+resu.to_csv(input+'_upcross.csv',sep=';')
