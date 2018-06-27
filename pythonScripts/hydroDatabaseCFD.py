431f7b65ddd75bacd039cba2f8abe72c996be266
qitjofivv <qitjofivv@login02.(none)>
2018-06-27 16:05:15 +0200

    With correct paths to make it work on cluster

--- pythonScripts/hydroDatabaseCFD.py
+++ pythonScripts/hydroDatabaseCFD.py
@@ -4,20 +4,20 @@
 #                                                                       #
 #   This script aims at launching CFD computations                      #
 #   on the whole hydrodynamics database                                 #
+#                                                                       #
 #########################################################################
 
 import sys
-sys.path.append(r"C:\drsvn\drtools\hyel_db_git")
 from Sourcesv2.envSettings import env
 #Set environment
-env["hstar_path"] = r'C:\Program Files\Bureau Veritas\Hstar4Experts\v8.00\bin'
-env["database"] =    r'C:\drsvn\dr-tools\Ship_Database\database_argos'
-env["output_path"] = r'D:\Etudes\hydroDatabaseCFD'
+env["hstar_path"] = r'/data/I1608251/HydroStar/bin'
+env["database"] =    r'/data/I1608251/Ship_Database_june2018/database_argos'
+env["output_path"] = r'/home/qitjofivv/hydroDatabase'
 #env["hmr_path"] = r'D:\_code_drsvn\Homer2\fortran\
 
 from Sourcesv2.createHST import createHST, createHST4OpenFOAM
 from Sourcesv2.reg_code import code_to_reg
-from Sourcesv2.launchHMR import runFreq , runTime, createFreq, runSpec
+#from Sourcesv2.launchHMR import runFreq , runTime, createFreq, runSpec
 
 import os
 from os.path import join
@@ -72,20 +72,19 @@ for code , loading in lShips :
     reg = code_to_reg(code)
 
     #runFreq( code , loading, speed  , AmgParam = {"MPAR" : "50 20\n"}, wadim = [ 0.3, 2.4 , 0.3 ] , nbproc = 2 )
-
     #runSpec( code, loading, speed, config, option = "time" )
     #runSpec( code, loading, speed, config, option = "spec" )
     if (createMeshes == True):
-      createHST4OpenFOAM(reg, loading, database=r'C:\drsvn\dr-tools\Ship_Database\database_argos')
+      print (reg)
+      createHST4OpenFOAM(reg, loading, database=env["database"] )
+
       folder = join(env["output_path"], code, loading, r'Input_Files')
   
       OFfolder =  join(env["output_path"], code, loading, r'foamMesh')
       if not os.path.exists(OFfolder):
           os.makedirs(OFfolder)
   
-      #mesh=Mesh(r"Full.hst")
       mesh=Mesh(join(folder,loading+".hst"))
-      print (mesh)
       mesh.makeConformal(0)
       mesh.writeSTL(join(OFfolder,r"ship.stl"))
     
