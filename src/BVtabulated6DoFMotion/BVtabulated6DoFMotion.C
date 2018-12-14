dce1057f9251d8727fa6b63996cf6aa9cde6647c
qsoyeaxjr <qsoyeaxjr@login02.(none)>
2018-12-14 11:50:49 +0100

    add BV*TransformPoints BVtabulated6DoFMotion

---
+++ src/BVtabulated6DoFMotion/BVtabulated6DoFMotion.C
@@ -0,0 +1,178 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "BVtabulated6DoFMotion.H"
+#include "addToRunTimeSelectionTable.H"
+#include "Tuple2.H"
+#include "IFstream.H"
+#include "interpolateSplineXY.H"
+#include "mathematicalConstants.H"
+
+using namespace Foam::constant::mathematical;
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace solidBodyMotionFunctions
+{
+    defineTypeNameAndDebug(BVtabulated6DoFMotion, 0);
+    addToRunTimeSelectionTable
+    (
+        solidBodyMotionFunction,
+        BVtabulated6DoFMotion,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solidBodyMotionFunctions::BVtabulated6DoFMotion::BVtabulated6DoFMotion
+(
+    const dictionary& SBMFCoeffs,
+    const Time& runTime
+)
+:
+    solidBodyMotionFunction(SBMFCoeffs, runTime)
+{
+    read(SBMFCoeffs);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solidBodyMotionFunctions::BVtabulated6DoFMotion::~BVtabulated6DoFMotion()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::septernion
+Foam::solidBodyMotionFunctions::BVtabulated6DoFMotion::transformation() const
+{
+    scalar t = time_.value();
+
+    if (t < times_[0])
+    {
+        FatalErrorIn
+        (
+            "solidBodyMotionFunctions::BVtabulated6DoFMotion::transformation()"
+        )   << "current time (" << t
+            << ") is less than the minimum in the data table ("
+            << times_[0] << ')'
+            << exit(FatalError);
+    }
+
+    if (t > times_.last())
+    {
+        FatalErrorIn
+        (
+            "solidBodyMotionFunctions::BVtabulated6DoFMotion::transformation()"
+        )   << "current time (" << t
+            << ") is greater than the maximum in the data table ("
+            << times_.last() << ')'
+            << exit(FatalError);
+    }
+
+    translationRotationVectors TRV = interpolateSplineXY
+    (
+        t,
+        times_,
+        values_
+    );
+
+    // Convert the rotational motion from deg to rad
+    TRV[1] *= pi/180.0;
+
+    //quaternion R(TRV[1].x(), TRV[1].y(), TRV[1].z());
+    quaternion R = quaternion(vector(0, 0, 1), TRV[1].z());
+    R *= quaternion(vector(0, 1, 0), TRV[1].y());
+    R *= quaternion(vector(1, 0, 0), TRV[1].x());
+
+    septernion TR(septernion(CofG_ + TRV[0])*R*septernion(-CofG_));
+
+    Info<< "solidBodyMotionFunctions::BVtabulated6DoFMotion::transformation(): "
+        << "Time = " << t << " transformation: " << TR << endl;
+
+    return TR;
+}
+
+
+bool Foam::solidBodyMotionFunctions::BVtabulated6DoFMotion::read
+(
+    const dictionary& SBMFCoeffs
+)
+{
+    solidBodyMotionFunction::read(SBMFCoeffs);
+
+    // If the timeDataFileName has changed read the file
+
+    fileName newTimeDataFileName
+    (
+        fileName(SBMFCoeffs_.lookup("timeDataFileName")).expand()
+    );
+
+    if (newTimeDataFileName != timeDataFileName_)
+    {
+        timeDataFileName_ = newTimeDataFileName;
+
+        IFstream dataStream(timeDataFileName_);
+
+        if (dataStream.good())
+        {
+            List<Tuple2<scalar, translationRotationVectors> > timeValues
+            (
+                dataStream
+            );
+
+            times_.setSize(timeValues.size());
+            values_.setSize(timeValues.size());
+
+            forAll(timeValues, i)
+            {
+                times_[i] = timeValues[i].first();
+                values_[i] = timeValues[i].second();
+            }
+        }
+        else
+        {
+            FatalErrorIn
+            (
+                "solidBodyMotionFunctions::BVtabulated6DoFMotion::read"
+                "(const dictionary&)"
+            )   << "Cannot open time data file " << timeDataFileName_
+                << exit(FatalError);
+        }
+    }
+
+    SBMFCoeffs_.lookup("CofG") >> CofG_;
+
+    return true;
+}
+
+
+// ************************************************************************* //
