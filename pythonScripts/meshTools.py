33927c3877017d582eb72053585cdf71b2b9af19
abenhamou <alexis.benhamou@bureauveritas.com>
2018-12-14 16:52:40 +0100

    pythonScripts: read points in not compressed format

--- pythonScripts/meshTools.py
+++ pythonScripts/meshTools.py
@@ -1,14 +1,22 @@
-import re
+import os, re
 from io import StringIO
 import pandas as pd
 import gzip
 
 
-def readPoints(pointsFile):
+def readPoints(polyMeshDir):
     """Read points from a polymesh
     """
-    with gzip.open(pointsFile, "rb") as f : 
-        data = f.read().decode()
+    #points in compressed format (*.gz)
+    pointsFile = os.path.join(polyMeshDir,'points.gz')
+    if os.path.isfile(os.path.join(polyMeshDir,'points.gz'))
+        with gzip.open(pointsFile, "rb") as f : 
+            data = f.read().decode()
+    #points in ascii format (*.gz)
+    pointsFile = os.path.join(polyMeshDir,'points')
+    elif os.path.isfile(os.path.join(polyMeshDir,'points'))
+        with open(pointsFile,'r')
+            data = f.read()
     pattern = r"\(\n(.*)\)\n\)"
     s = StringIO( re.search(pattern, data, re.DOTALL).group(1).replace("(", "").replace(")", ""))
     return pd.read_csv(s, delim_whitespace = True, header = None, names = ["x", "y", "z"] )
